15bc6968105b20bd5d4b8fb1b191284ae9670ea5
Sunny Gupta <sunnymax2002@gmail.com>
2021-05-16 16:11:38 +0530

    minor changes, still same functionality

--- esp32/esp32_ble.py
+++ esp32/esp32_ble.py
@@ -6,10 +6,10 @@ import ubluetooth
 from esp32 import raw_temperature
 
 class BLE():
-
-
     def __init__(self, name):
-        
+        # Stores connection status
+        self._status = False
+    
         self.name = name
         self.ble = ubluetooth.BLE()
         self.ble.active(True)
@@ -22,27 +22,24 @@ class BLE():
         self.ble.irq(self.ble_irq)
         self.register()
         self.advertiser()
-
-
+    
     def connected(self):
-        
         self.timer1.deinit()
         self.timer2.deinit()
         print("connected!")
-        #self.send("hello from ESP32")
-
-
+        self._status = True
 
     def disconnected(self):
-        
         self.timer1.init(period=1000, mode=Timer.PERIODIC, callback=lambda t: self.led(1))
         sleep_ms(200)
         self.timer2.init(period=1000, mode=Timer.PERIODIC, callback=lambda t: self.led(0))
-    
+        self._status = False
 
     def ble_irq(self, event, data):
+        # Add code here to handle BLE events (Connect, Disconnect. Rx)
+        
         print("ble_irq")
-        print(event)
+        print("BLE event: " + str(event))
         
         if event == 1:
             '''Central disconnected'''
@@ -56,19 +53,14 @@ class BLE():
         
         elif event == 3:
             '''New message received'''
-            
             buffer = self.ble.gatts_read(self.rx)
-            message = buffer.decode('UTF-8')[:-1]
-            print(message)
+            message = buffer.decode('UTF-8').strip()
+            #[:-1]
             
+            print("BLE received message: " + message)
             self.send("hello")
             
-            #if message == 'blue_led':
-            #    blue_led.value(not blue_led.value())
-
-            
     def register(self):
-        
         # Nordic UART Service (NUS)
         NUS_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
         RX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
@@ -82,15 +74,19 @@ class BLE():
         SERVICES = (BLE_UART, )
         ((self.tx, self.rx,), ) = self.ble.gatts_register_services(SERVICES)
 
-
     def send(self, data):
-        self.ble.gatts_notify(0, self.tx, data + '\n')
-
+        if self._status:
+            print("sending data...")
+            self.ble.gatts_notify(0, self.tx, data + '\n')
+        else:
+            print("first connect then send")
 
     def advertiser(self):
         name = bytes(self.name, 'UTF-8')
+        # TODO: change BLE advertise gap duration
         self.ble.gap_advertise(100, bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name)
-        
+            
 # test
 blue_led = Pin(2, Pin.OUT)
 ble = BLE("ESP32")
+ble.send("1st msg")
