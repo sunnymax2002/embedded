d292c5e34f55db7cb76809850748c5c810e8b7ff
Sunny Gupta <sunnymax2002@gmail.com>
2021-05-16 15:29:15 +0530

    initial code working, able to fetch http data from URLs

---
+++ esp32/esp32_wifi_connect.py
@@ -0,0 +1,36 @@
+import machine
+import network
+import socket
+
+# Try: https://microcontrollerslab.com/esp32-esp8266-micropython-web-server/
+
+def http_get(url):
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+    
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('ACT102504171662', '68170929')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+    # turn LED on
+    led = machine.Pin(2, machine.Pin.OUT)
+    # led.on()
+    
+do_connect()
+http_get('http://micropython.org/ks/test.html')
+http_get('http://lite.cnn.com/en')
