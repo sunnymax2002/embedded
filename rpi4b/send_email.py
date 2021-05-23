1caac6e717e511bd07b0c78833ae328afb90c10f
Sunny Gupta <sunnymax2002@gmail.com>
2021-05-23 17:57:40 +0530

    created scripts to check if vncserver has started and send email to update status

---
+++ rpi4b/send_email.py
@@ -0,0 +1,39 @@
+# REF: https://bc-robotics.com/tutorials/sending-email-using-python-raspberry-pi/
+
+import smtplib
+ 
+#Email Variables
+SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
+SMTP_PORT = 587 #Server Port (don't change!)
+GMAIL_USERNAME = 'sunnyraspi64@gmail.com' #change this to match your gmail account
+GMAIL_PASSWORD = 'ju9kyard'  #change this to match your gmail password
+ 
+class Emailer:
+    def sendmail(self, recipient, subject, content):
+         
+        #Create Headers
+        headers = ["From: " + GMAIL_USERNAME, "Subject: " + subject, "To: " + recipient,
+                   "MIME-Version: 1.0", "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+ 
+        #Connect to Gmail Server
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+ 
+        #Login to Gmail
+        session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+ 
+        #Send Email & Exit
+        session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + content)
+        session.quit
+ 
+sender = Emailer()
+ 
+sendTo = 'sunnymax2002@gmail.com'
+emailSubject = "Hello from sunnyraspi64"
+emailContent = "Sunny's raspi [6] 4 is ready to be connected via SSH/VNC"
+ 
+#Sends an email to the "sendTo" address with the specified "emailSubject" as the subject and "emailContent" as the email content.
+sender.sendmail(sendTo, emailSubject, emailContent)
